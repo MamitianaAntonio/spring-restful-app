3b928631473ea5b63a312b405e971e4d30ad7c67
Mamitiana Antonio <ratolojanaharyantonio1@gmail.com>
2025-12-10 21:15:35 +0300

    first commit

---
+++ src/main/java/com/antonio/restful_api/RestfulApiApplication.java
@@ -0,0 +1,11 @@
+package com.antonio.restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+}
